580278d560bddec1f128c41d003857af1b8b6335
@guyaldous1 <guyaldous@gmail.com>
2021-03-30 12:25:09 +1100

    restructure gulp

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,35 @@
+import './_app/polyfills'
+import {waitForFinalEvent, matches} from "./_app/helpers.js"
+// import "smooth-scroll";
 
-var donk = () => {
-  console.log('home is here');
+//run all init scripts
+const loadHandler = () => {
+
+  // //smooths scrollings
+  // new SmoothScroll('a[href*="#"]', {
+  //   speed: 1000,
+  //   offset: 50,
+  //   easing: 'easeInOutCubic',
+  // });
+
+  document.querySelector('body').classList.add('loaded');
+
 }
 
-donk();
+document.addEventListener("DOMContentLoaded", loadHandler)
+
+
+//run all scripts on resize
+const resizeHandler = () => waitForFinalEvent(() => {
+
+
+
+}, 100, 'dont resize again');
+window.addEventListener('resize', resizeHandler)
+
+
+//event bubbling click handler
+const clickHandler = () => {
+
+}
+document.addEventListener('click', clickHandler, false);
